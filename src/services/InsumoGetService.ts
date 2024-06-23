50db30bd944ef22720965b013c0b165ef3dcc18f
MaxiColombo <maximocolombo2003@gmail.com>
2024-06-23 19:44:29 -0300

    fix

--- src/services/InsumoGetService.ts
+++
@@ -1,9 +0,0 @@
-import IArticuloInsumo from "../types/ArticuloInsumo";
-
-import { BackendClient } from "./BackendClient;
-
-// Clase PersonaService que extiende BackendClient para interactuar con la API de personas
-export class InsumoGetService extends BackendClient<IArticuloInsumo> {
-
-
-}
